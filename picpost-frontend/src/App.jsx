3cb800e4ae2447760ae5caa7bdae9fbc32ab37e2
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2025-04-08 16:12:10 +0100

    Added basic image and caption form

--- picpost-frontend/src/App.jsx
+++ picpost-frontend/src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [image, setImage] = useState(null);
+  const [caption, setCaption] = useState('');
+
+  const handleSubmit = (e) => {
+    e.handleSubmit();
+    console.log({image, caption})
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 p-6'>
+      <form onSubmit={handleSubmit} className='bg-white p-8 rounded-2xl shadow-md w-full max-w-md space-y-4'>
+        <h1 className='text-2xl font-bold mb-4 text-center'>📸 PicPost</h1>
+        <input
+        type='file' accept='image/*' onChange={(e) => setImage(e.target.files[0])} className='w-full border rounded px-3 py-2'
+        required
+        />
+        
+        <textarea
+        placeholder='Write a caption...' value={caption} onChange={(e) => setCaption(e.target.value)} className='w-full border rounded px-3 py-2' rows='3'
+        required
+        ></textarea>
+
+        <button type='submit' className="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600 transition">
+          Upload
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+    </div>
   )
 }
 
