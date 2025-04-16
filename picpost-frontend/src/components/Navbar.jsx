415d11642fb108a27cf5d25d719da90f5114b601
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2025-04-16 14:06:35 +0100

    Fix navbar alignment and logo placement

--- picpost-frontend/src/components/Navbar.jsx
+++ picpost-frontend/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import logo from '../assets/logo.jpg';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className='bg-black text-white px-6 py-4 flex items-center justify-between shadow-md'>
-        <h1 className='text-xl font-bold text-pink-500'>OMAV</h1>
-        <button className='bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600 transition'>
-            Profile
-        </button>
-      </nav>
+    <nav className="bg-black text-white px-6 py-4 w-full">
+    <div className="flex items-center justify-between w-full">
+      <div className="flex items-center space-x-2">
+        <img src={logo} alt="PicPost" className="h-6 w-auto object-contain" />
+        <span className="text-pink-500 font-bold text-xl">OMAV</span>
+      </div>
+      <button className="bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600 transition">
+        Profile
+      </button>
     </div>
+  </nav>
   )
 }
 
