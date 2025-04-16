732e9c20768ff9a49e814eeb739ce21bea301d31
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2025-04-16 18:05:45 +0100

    Add user login route

--- picpost-backend/routes/authRoutes.js
+++ picpost-backend/routes/authRoutes.js
@@ -13,7 +13,6 @@ router.post('/register', async (req, res) => {
 
     try {
         const existingUser = await User.findOne({email});
-
         if (existingUser) return res.status(400).json({message: 'email already in use'});
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -28,4 +27,34 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         res.status(500).json({message: 'registration failed!', error: error.message })
     }
-})
+});
+
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if (!user) return res.status(404).json({message: 'user not found'});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({message: 'invalid credentials'})
+        
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+        });
+
+        res.status(200).json({
+            message: 'login successful',
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+        })
+    } catch (error) {
+        res.status(500).json({message: 'login failed', error: error.message});
+    }
+});
+
+export default router;
