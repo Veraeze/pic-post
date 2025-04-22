79383697d26de7b97b2d50221be5cdebf6c7b644
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2025-04-22 19:34:33 +0100

    Add user reference to posts and following field to users

--- picpost-backend/models/postModel.js
+++ picpost-backend/models/postModel.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose';
 const postSchema = new mongoose.Schema({
   caption: String,
   imageUrl: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
--- picpost-backend/models/userModel.js
+++ picpost-backend/models/userModel.js
@@ -19,6 +19,12 @@ const userSchema = new mongoose.Schema(
             required: true,
             minlength: 7,
         },
+        following: [
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            }
+        ],
     },
     {timestamps: true}
 );
